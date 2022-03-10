8a58a05512ac928942cb56620bf5408e1e800ff0
echoeyecodes <femiobajuluwa@gmail.com>
2022-03-10 20:07:56 +0100

    1. cols represented as width, rows represented as height 2. mapping pixels to intarrays to reduce time on calling getPixels

--- app/src/main/java/com/echoeyecodes/testproject/activities/ImageActivity.kt
+++ app/src/main/java/com/echoeyecodes/testproject/activities/ImageActivity.kt
@@ -37,34 +37,38 @@ class ImageActivity : AppCompatActivity() {
             floatArrayOf(1 / 9f, 1 / 9f, 1 / 9f, 1 / 9f, 1 / 9f, 1 / 9f, 1 / 9f, 1 / 9f, 1 / 9f)
         withContext(Dispatchers.IO) {
             val diameter = 3 // 3x3 matrix
-            val copyBitmap = bitmap.copy(bitmap.config, true)
-            val rows = bitmap.width
-            val cols = bitmap.height
+            val cols = bitmap.width
+            val rows = bitmap.height
             val step = (diameter - 1) / 2
 
-            for (i in 0 until rows) {
-                for (j in 0 until cols) {
+            val imagePixels = IntArray(rows * cols)
+            val newImagePixels = IntArray(rows * cols)
+
+            bitmap.getPixels(imagePixels, 0, cols, 0, 0, cols, rows)
+            for (i in 0 until cols) {
+                for (j in 0 until rows) {
                     val startCoord = Point(i - step, j - step)
                     val endCoord = Point(i + step, j + step)
 
                     val matrix = ArrayList<Int>()
-                    for (_row in startCoord.x..endCoord.x) {
-                        for (_col in startCoord.y..endCoord.y) {
+                    for (_col in startCoord.x..endCoord.x) {
+                        for (_row in startCoord.y..endCoord.y) {
                             if (_row < 0 || _row >= rows || _col < 0 || _col >= cols) {
                                 //out of matrix bounds
                                 matrix.add(0)
                             } else {
-                                val pixel = bitmap.getPixel(_row, _col)
+                                val pixel = imagePixels[_row * cols + _col]
                                 matrix.add(pixel)
                             }
                         }
                     }
-                    val sum = matrix.mapIndexed { index, i -> kernel[index] * i }.sum()
+                    val sum = matrix.mapIndexed { index, value -> kernel[index] * value }.sum()
                     val divisor = kernel.sum()
                     val average = (sum / divisor).toInt()
-                    copyBitmap.setPixel(i, j, average)
+                    newImagePixels[j * cols + i] = average
                 }
             }
+            val copyBitmap = Bitmap.createBitmap(newImagePixels, cols, rows, Bitmap.Config.ARGB_8888)
             runOnUiThread {
                 Glide.with(this@ImageActivity).load(copyBitmap).into(imageView)
             }
